89494e512c03fdfe6ab5c41e0a388b380f24dc4d
SangeethKumarPG <101401368+SangeethKumarPG@users.noreply.github.com>
2022-03-14 21:19:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function compute()
     if(principal <= 0){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
-        
+        return ;
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var year = new Date().getFullYear()+parseInt(years);
     var interest = principal * years * rate / 100;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%.\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
 
 
 
